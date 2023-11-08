adeff02cda1eceac2df455014eb09cbc8b54589b
AbdelrahmanAhmed605 <abdelrahman.ahmed605@hotmail.com>
2023-11-08 17:12:38 -0500

    Added validation checks for budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,23 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, dispatch } = useContext(AppContext);
-    // new state with initial value of current budget
-    const [newBudget, setNewBudget] = useState(budget);
+    const { budget, expenses, dispatch } = useContext(AppContext);
+    const maxBudget = 20000; // Maximum value of budget
+
     const handleBudgetChange = (event) => {
+        const newBudget = event.target.value;
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+        
+        if(parseInt(newBudget) > 20000) {
+            alert("Budget cannot exceed "+maxBudget);
+            return;
+        }
+        if(parseInt(newBudget) < totalExpenses) {
+            alert("You cannot reduce the budget lower than spending ");
+            return;
+        }
         dispatch({
             type: 'SET_BUDGET',
             payload: event.target.value,
@@ -14,7 +27,7 @@ const Budget = () => {
     return (
 <div className='alert alert-secondary'>
 <span style={{ paddingRight: '0.75rem' }}>Budget: £{budget}</span>
-<input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" max={maxBudget} value={budget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
